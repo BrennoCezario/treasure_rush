cabab085e18a9bf88efe6c145d8dc0559a6916ae
Brenno Cezário <cezariobrenno@gmail.com>
2024-11-23 20:49:23 -0300

    Alteração de estrutura e mapa do jogo + player inserido na tela

--- main.py
+++ main.py
@@ -1,108 +1,87 @@
-# Configurações iniciais
-
-# Criar Loop Infinito 
-
-# Desenhar os objetos do jogo na tela
-# personagem do jogador 
-# pontuação
-# tesouro
-
-# Desenhar Mapa do jogo na tela
-# mapa principal
-# mapa do tesouro
-
-# Criar Lógica de iniciar a partida
-# Requisitos:
-# sala existente
-# sala com host 
-# sala com no mínimo 2 jogadores e no máximo 4
-# jogadores não hosts precisam permitir o início
-# Para iniciar:
-# Host inicia partida
-        
-# Criar Lógica de terminar a partida
-# todos os tesouros são coletados
-# resta um único jogador na partida
-
-# Criar Lógica de finalizar o client
-# tecla "X" é clicada
-# opção sair do jogo no menu principal é acionado
-
-# Interações do usuário
-# minimizar, ampliar e fechar tela
-# opções de menu e pause durante o jogo
-# comandos básicos
-
 import pygame
 
-pygame.init()
-pygame.display.set_caption("Treasure Rush")
-cell_size = 32  
-grid_size = 16
-width, height = cell_size * grid_size, cell_size * grid_size
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
+cell_size = 32
+grid_size = 16  
 
-# Cores (RGB)
-black_color = (0, 0, 0)
-white_color = (255, 255, 255)
-red_color = (255, 0, 0)
-green_color = (0, 255, 0)
-blue_color= (0, 0, 255)
-background_color = (175, 196, 230)
-wall_color = (128, 99, 69)
-border_color = (210, 210, 210)
+game_over = False
+  
+class Player:
+    def __init__(self) -> None:
+        self.position = pygame.Rect(7*cell_size + cell_size // 4, 7*cell_size + cell_size // 4, grid_size, grid_size)
+        
+    def move(self, dx, dy, main_map):
+        new_x = self.position.x + dx * cell_size
+        new_y = self.position.y + dy * cell_size
 
-# Mapa Principal
-main_map = [[0 for _ in range(16)] for _ in range(16)]
-for i in range(16):
-    for j in range(16):
-        if i == 0 or j == 0 or i == 15 or j == 15:
-            main_map[i][j] = 1
-    
-def draw_map():
-    for i in range(grid_size):
-        for j in range(grid_size):
-            color = wall_color if main_map[i][j] == 1 else background_color
-            border = wall_color if main_map[i][j] == 1 else border_color
+        grid_x = new_x // cell_size
+        grid_y = new_y // cell_size
+
+        if not main_map.is_wall(grid_y, grid_x):
+            self.position.x = new_x
+            self.position.y = new_y
+        
+    def draw_player(self):
+        pygame.draw.rect(surface= screen, color= "white", rect= self.position)
+
+
+class MainMap:
+    def __init__(self) -> None:
+        self.space = [[0 for i in range(16)]for i in range(16)]
+        for i in range(16):
+            for j in range(16):
+                if i == 0 or j == 0 or i == 15 or j == 15:
+                    self.space[i][j] = 1 
+                     
+    def draw_map(self):
+        for i in range(16):
+            for j in range(16):
+                color = (150, 120, 70) if self.space[i][j] == 1 else (200, 180, 100)
+                border = (100, 80, 50) if self.space[i][j] == 1 else (170, 150, 90)
             
-            x = i * cell_size
-            y = j * cell_size
-            pygame.draw.rect(
-                    screen,
-                    color,
-                    (j * cell_size, i * cell_size, cell_size, cell_size),
-                )
-            pygame.draw.rect(
-                    screen,
-                    border,
-                    (j * cell_size, i * cell_size, cell_size, cell_size),
-                    1
-                )
-            
-def draw_player(player_position):
-    row, col = player_position
-    pygame.draw.rect(
-        screen,
-        black_color,
-        (col * cell_size, row * cell_size, cell_size, cell_size)
-    )
+                pygame.draw.rect(
+                        screen,
+                        color,
+                        (j * cell_size, i * cell_size, cell_size, cell_size),
+                    )
+                pygame.draw.rect(
+                        screen,
+                        border,
+                        (j * cell_size, i * cell_size, cell_size, cell_size),
+                        1
+                    )
+    def is_wall(self, row, col):
+        return self.space[row][col] == 1
 
-def run_game():
-    game_over = False
-    
-    draw_map()
-    draw_player([7, 7])
+def event_listener(player, main_map):
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                global game_over
+                game_over = True
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    player.move(0, 1, main_map)
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    player.move(0, -1, main_map)
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    player.move(-1, 0, main_map)
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    player.move(1, 0, main_map)
+
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((grid_size*cell_size, grid_size*cell_size))
+    pygame.display.set_caption("Treasure Rush")
+
+    player = Player()
+    main_map = MainMap()
     
     while not game_over:
-
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                draw_player([8,7])
-            
-            if event.type == pygame.QUIT:
-                game_over = True
-
-run_game()
+        event_listener(player, main_map)
+        screen.fill("black")
+        main_map.draw_map()
+        player.draw_player()
+        pygame.display.flip()
+    
+    pygame.quit()
